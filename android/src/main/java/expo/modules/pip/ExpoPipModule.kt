afb53292612f76285c18a041fe033bdb8a1c513d
Spharian <s.fertons@gmail.com>
2024-04-03 12:02:26 +0200

    app context

--- android/src/main/java/expo/modules/pip/ExpoPipModule.kt
+++ android/src/main/java/expo/modules/pip/ExpoPipModule.kt
@@ -2,12 +2,11 @@ package expo.modules.pip
 
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
-import expo.modules.kotlin.AppContext
 import android.os.Build
 import android.util.Rational
 import android.app.PictureInPictureParams
 
-class ExpoPipModule(appContext: AppContext) : Module() {
+class ExpoPipModule() : Module() {
     // Each module class must implement the definition function. The definition consists of components
     // that describe the module's functionality and behavior.
     // See https://docs.expo.dev/modules/module-api for more details about available components.
