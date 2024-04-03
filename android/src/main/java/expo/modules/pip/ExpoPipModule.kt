2ed3d8ab67b4cc92a5fa9f97848bbc8a824282f7
Spharian <s.fertons@gmail.com>
2024-04-03 11:24:51 +0200

    fix activity

--- android/src/main/java/expo/modules/pip/ExpoPipModule.kt
+++ android/src/main/java/expo/modules/pip/ExpoPipModule.kt
@@ -2,11 +2,12 @@ package expo.modules.pip
 
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
+import expo.modules.kotlin.AppContext
 import android.os.Build
 import android.util.Rational
 import android.app.PictureInPictureParams
 
-class ExpoPipModule : Module() {
+class ExpoPipModule(appContext: AppContext) : Module() {
     // Each module class must implement the definition function. The definition consists of components
     // that describe the module's functionality and behavior.
     // See https://docs.expo.dev/modules/module-api for more details about available components.
@@ -22,7 +23,7 @@ class ExpoPipModule : Module() {
                 val params = PictureInPictureParams.Builder()
                     .setAspectRatio(Rational(3, 4))
                     .build()
-                getCurrentActivity()?.enterPictureInPictureMode(params)
+                appContext.currentActivity?.enterPictureInPictureMode(params)
             }
         }
     }
