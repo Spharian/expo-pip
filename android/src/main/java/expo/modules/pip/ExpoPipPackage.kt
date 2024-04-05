37308a8f31dc4602a57f31228961fd66b860d810
Spharian <s.fertons@gmail.com>
2024-04-05 14:35:26 +0200

    conitnue

--- android/src/main/java/expo/modules/pip/ExpoPipPackage.kt
+++ android/src/main/java/expo/modules/pip/ExpoPipPackage.kt
@@ -4,9 +4,14 @@ import android.content.Context
 
 import expo.modules.core.interfaces.InternalModule
 import expo.modules.core.interfaces.Package
+import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
+import androidx.core.os.bundleOf
 
 class ExpoPipPackage : Package {
-  override fun createReactActivityLifecycleListeners(activityContext: Context): List<ReactActivityLifecycleListener> {
-    return listOf(ExpoPipActivityLifecycleListener())
-  }
+   inner class ExpoPipAppCompatActivity : AppCompatActivity() {
+        override fun onPictureInPictureModeChanged(isInPictureInPictureMode: Boolean, newConfig: Configuration) {
+            ExpoPipModule().sendEvent("onPictureInPictureModeChange", bundleOf("isInPictureInPictureMode" to isInPictureInPictureMode))
+        }
+    }
 }
